cfd8fff196e921ae6ece992a4630ae3b7d8161a5
satpai1215 <pai.satyan@gmail.com>
2014-06-28 14:59:10 -0700

    update gem with guard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def full_title(page_title)
+  	base_title = "Ruby on Rails Tutorial Sample App"
+  	if page_title.empty?
+  		base_title
+  	else
+  		"#{base_title} | #{page_title}"
+  	end
+  end
 end
